4c043065fd92baf48c85f40c51d87007ec88a8c4
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2025-11-10 17:37:47 +0530

    Update and rename 12.Bipartite Graph to 12.Bipartite Graph.py

--- 12.Bipartite Graph.py
+++ 12.Bipartite Graph.py
@@ -1,30 +1,33 @@
 problem link : https://www.geeksforgeeks.org/problems/bipartite-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bipartite-graph
 
-from collections import  deque
+from collections import deque
 class Solution:
-	def isBipartite(self, V, adj):
+    def isBipartite(self, V, edges):
         visited = [-1] * V 
+        graph = [[] for _ in range(V)]
+        
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
         
         for i in range(V):
             if visited[i] == -1 :
-                if self.bfs(i, 0, adj, visited) == 0:
+                if self.bfs(i, 0, graph, visited) == 0:
                     return 0 
-        
         return 1 
     
-    def bfs(self, node, color, adj, visited):
+    def bfs(self, node, color, graph, visited):
         q = deque([(node, color)])
         
         while q:
             node, color = q.popleft()
             visited[node] = color 
             
-            for adjNode in adj[node]:
+            for adjNode in graph[node]:
                 if visited[adjNode] == -1:
                     q.append((adjNode, 1-color))
                 elif visited[adjNode] == color:
                     return 0 
-        
         return 1
 
 ''' time complexity : O(V + 2E)
