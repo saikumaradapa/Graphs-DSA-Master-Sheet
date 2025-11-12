eb6871871f3e63fc3693190c045759ed3defdf47
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2025-11-12 18:00:41 +0530

    Update and rename 21.Implementing Dijkstra Algorithm to 21.Implementing Dijkstra Algorithm.py

--- 21.Implementing Dijkstra Algorithm.py
+++ 21.Implementing Dijkstra Algorithm.py
@@ -2,25 +2,28 @@ problem link : https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-
 
 from heapq import heapify, heappop, heappush
 class Solution:
-
-    def dijkstra(self, V, adj, S):
-        res = [float('inf')] * V
-        res[S] = 0 
+    def dijkstra(self, V, edges, src):
+        graph = [[] for _ in range(V)]
+        for u, v, w in edges:
+            graph[u].append((v, w))
+            graph[v].append((u, w))
         
-        heap = [(0, S)] # dist, node
+        res = [float("inf")] * V
+        res[src] = 0
+        
+        heap = [(0, src)] # (dist, node)
         heapify(heap)
         
         while heap:
             dist, node = heappop(heap)
-            for curr in adj[node]:
+            for curr in graph[node]:
                 currDist = dist + curr[1]
                 
                 if currDist < res[curr[0]]:
                     res[curr[0]] = currDist
                     heappush(heap, (currDist, curr[0]))
-        
         return res
-
+        
 ''' time complexity : O(E log V)
     space complexity : O(V)
-'''
+'''        
