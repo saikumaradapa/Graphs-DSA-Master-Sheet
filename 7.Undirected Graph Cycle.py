57f2d723206f6fe3759c7dc21f285db5ea4e863e
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-08-24 16:37:44 +0530

    Refactor cycle detection in undirected graph
    Refactor DFS cycle detection logic and improve readability.

--- 7.Undirected Graph Cycle.py
+++ 7.Undirected Graph Cycle.py
@@ -6,29 +6,27 @@ class Solution:
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
+
         visited = [0] * V
         for node in range(V):
-            if visited[node] == 0:
+            if not visited[node]:
                 if self.dfs(node, visited, graph, -1):
                     return True
         return False
 
     def dfs(self, node, visited, graph, parent):
-        if visited[node] == 0:
-            visited[node] = 1
-            for adjNode in graph[node]:
-                if visited[adjNode] == 1:
-                    if adjNode != parent:
-                        return True
-                else:
-                    if self.dfs(adjNode, visited, graph, node):
-                        return True
-        return False 
-        
-        
-''' DFS 
-    time complexity : O(V + 2E)
-    space complexity : O(V)
+        visited[node] = 1
+        for adjNode in graph[node]:
+            if not visited[adjNode]:
+                if self.dfs(adjNode, visited, graph, node):
+                    return True
+            elif adjNode != parent:   # visited & not parent -> cycle
+                return True
+        return False
+
+''' DFS
+    time  : O(V + 2E)
+    space : O(V)   visited + recursion stack
 '''
 
 
