2ba37f26eb5307bae62b532baa642f0025f2e3d0
Sai Kumar Adapa <96902883+saikumaradapa@users.noreply.github.com>
2026-08-25 19:26:03 +0530

    Update 25.Cheapest Flights Within K Stops.py

--- 25.Cheapest Flights Within K Stops.py
+++ 25.Cheapest Flights Within K Stops.py
@@ -9,25 +9,25 @@ class Solution:
         graph = [[] for _ in range(n)]
         for u, v, w in flights:
             graph[u].append((v, w))
-        
+
         cost = [float("inf")] * n 
         cost[src] = 0
-        
+
         q = deque([(0, src, 0)]) # stops, node, cost
         while q:
             stops, node, price = q.popleft()
             if stops > k:
                 continue
-            
+
             for curr in graph[node]:
                 adjNode = curr[0]
                 adjWt = curr[1]
-                if cost + adjWt < cost[adjNode] and stops <= k:
-                    cost[adjNode] = cost + adjWt
-                    q.append((stops + 1, adjNode, cost + adjWt))
-                    
+                if price + adjWt < cost[adjNode] and stops <= k:
+                    cost[adjNode] = price + adjWt
+                    q.append((stops + 1, adjNode, price + adjWt))
+
         return cost[dst] if cost[dst] != float("inf") else -1
-        
+
 ''' can be solved with priority queue (heap) with dijkstra algorithm which cost extra log time
     time complexity : O(E) - at worst all edges will be consider once
     space complexity : O(E)
